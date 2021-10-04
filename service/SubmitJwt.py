ea53c291f5236f524070f07fd27c80b957710081
Tarjei Skrede <tarjeiskrede@M-C02Y7599JGH6.local>
2021-10-04 16:07:34 +0200

    removed microservice, only kept readme

--- service/SubmitJwt.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-
-class SubmitJwt(FlaskForm):
-    jwt = TextAreaField('jwt', validators=[DataRequired()])
-    submit = SubmitField('Submit')
